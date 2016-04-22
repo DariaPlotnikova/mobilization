05deaa4752dd1f5d0b77dbe374ea81f96481ba1e
daria <plotnikovanstu@gmail.com>
2016-04-22 11:16:23 +0600

    Initial commit

---
+++ app/src/main/java/test/mobi/mobilizationtest/ArtistListFragment.java
@@ -0,0 +1,22 @@
+package test.mobi.mobilizationtest;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistListFragment extends Fragment {
+
+    public ArtistListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_artist_list, container, false);
+    }
+}
