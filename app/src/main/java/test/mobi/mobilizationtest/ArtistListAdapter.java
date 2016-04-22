d799f0a2d9b0458ec996275777426b583c1f7959
daria <plotnikovanstu@gmail.com>
2016-04-22 12:59:05 +0600

    ArtistList adapter's methods defined

--- app/src/main/java/test/mobi/mobilizationtest/ArtistListAdapter.java
+++ app/src/main/java/test/mobi/mobilizationtest/ArtistListAdapter.java
@@ -1,12 +1,18 @@
 package test.mobi.mobilizationtest;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
+
+import static test.mobi.mobilizationtest.R.*;
+import static test.mobi.mobilizationtest.R.string.artist_img;
+import static test.mobi.mobilizationtest.R.string.works_in_list;
 
 /**
  * Created by daria on 22.04.16.
@@ -23,21 +29,40 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Vi
      * Создание новых View и ViewHolder элементов списка
      */
     @Override
-    public ArtistListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(layout.artistlist_item, parent, false);
+        return new ViewHolder(v);
     }
 
     /**
      * Заполнение виджетов данными из элемента artists[i]
      */
     @Override
-    public void onBindViewHolder(ArtistListAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Artist curArtist = artists.get(position);
+        int imageId = 0;
+        holder.image.setImageResource(imageId);
+        holder.name.setText(curArtist.getName());
+        holder.style.setText(curArtist.getStyle());
+        holder.works.setText(works_in_list);
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    private void copy(Artist artist){
+        int position = artists.indexOf(artist);
+        Artist copy = artist.copy();
+        artists.add(position + 1, copy);
+        notifyItemInserted(position + 1);
+    }
+
+    private void delete(Artist artist){
+        int position = artists.indexOf(artist);
+        artists.remove(position);
+        notifyItemRemoved(position);
     }
 
     /**
@@ -51,10 +76,10 @@ public class ArtistListAdapter extends RecyclerView.Adapter<ArtistListAdapter.Vi
 
         public ViewHolder(View itemView) {
             super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.artistListItemImage);
-            name = (TextView) itemView.findViewById(R.id.artistListItemName);
-            style = (TextView) itemView.findViewById(R.id.artistListItemStyle);
-            works = (TextView) itemView.findViewById(R.id.artistListItemWorks);
+            image = (ImageView) itemView.findViewById(id.artistListItemImage);
+            name = (TextView) itemView.findViewById(id.artistListItemName);
+            style = (TextView) itemView.findViewById(id.artistListItemStyle);
+            works = (TextView) itemView.findViewById(id.artistListItemWorks);
         }
     }
 }
