b6f3244519576ae086f35c335a7bdd34424f2f4b
daria <plotnikovanstu@gmail.com>
2016-04-23 11:36:22 +0600

    Artist class changed to store full data from JSON

--- app/src/main/java/test/mobi/mobilizationtest/Artist.java
+++ app/src/main/java/test/mobi/mobilizationtest/Artist.java
@@ -6,17 +6,21 @@ import android.media.Image;
  * Created by daria on 22.04.16.
  */
 public class Artist {
-    private Image image;
+    private long id;
     private String name;
     private String style;
     private int albums;
     private int songs;
+    private String link;
+    private String description;
+    private String coverSmall;
+    private String coverBig;
 
-    public Image getImage() {
-        return image;
+    public long getId() {
+        return id;
     }
-    public void setImage(Image image) {
-        this.image = image;
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -47,13 +51,45 @@ public class Artist {
         this.songs = songs;
     }
 
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCoverSmall() {
+        return coverSmall;
+    }
+    public void setCoverSmall(String coverSmall) {
+        this.coverSmall = coverSmall;
+    }
+
+    public String getCoverBig() {
+        return coverBig;
+    }
+    public void setCoverBig(String coverBig) {
+        this.coverBig = coverBig;
+    }
+
     public Artist copy(){
         Artist copyArtist = new Artist();
-        copyArtist.setImage(image);
+        copyArtist.setId(id);
         copyArtist.setName(name);
         copyArtist.setStyle(style);
         copyArtist.setAlbums(albums);
         copyArtist.setSongs(songs);
+        copyArtist.setLink(link);
+        copyArtist.setDescription(description);
+        copyArtist.setCoverSmall(coverSmall);
+        copyArtist.setCoverBig(coverBig);
         return copyArtist;
     }
 }
